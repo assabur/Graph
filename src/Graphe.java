1b75a811d552c2c87189a6e0e0681f6208cd662e
Only_soft <abdullahwin99@gmail.com>
2019-12-10 01:07:36 +0100

    Part2 algo BFS

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.swing.text.html.HTMLDocument.Iterator;
+
 
 public class Graphe 
 {
@@ -13,11 +14,23 @@ public class Graphe
 	 * Dans ce exercice le graphe est considere non oriente
 	 * 
 	 * */
-	
+	private LinkedList<Integer> adjancence[]; //elle correspond à ma liste d'adjencence
+	private static final int V = 0;
 	private int nbre_sommet;
+	
+	public int getNbre_sommet() {
+		return nbre_sommet;
+	}
+
+	public void setNbre_sommet(int nbre_sommet) {
+		this.nbre_sommet = nbre_sommet;
+	}
+
+
+
 	private ArrayList<LinkedList<Integer>> mesElements=new ArrayList<LinkedList<Integer>>();
 	 
-	 //j'initialise mon graphe avec n sommet 
+	 //j'initialise mon graphe avec n sommet de valeur i allant de 0 à n
 	
 	
 	public Graphe(int n) 
@@ -27,8 +40,17 @@ public class Graphe
         throw new IllegalArgumentException("le nombre de sommets ne peut etre inferieur ou egal à zero");
         this.nbre_sommet = n;
         this.mesElements = new ArrayList<LinkedList<Integer>>(nbre_sommet);
+        
+        
         for (int i = 0; i < n; i++)
-            mesElements.add(new LinkedList<Integer>());
+        { 
+        	LinkedList<Integer> a=new LinkedList<Integer>();
+        	mesElements.add(new LinkedList<Integer>());
+        	a.add(i);
+        	mesElements.add(a);
+        }  
+        
+       
         
         // jai desormais  n sommets dans mon graphe contenant de 0 à n comme valeur de sommet
     }
@@ -39,11 +61,12 @@ public class Graphe
 	
 	public void ajouteArete(int sommet_i, int sommet_j)
 	{
-        
-        verifier_sommet(sommet_i);
-        verifier_sommet(sommet_j);
+        if (verifier_sommet(sommet_i )&& verifier_sommet(sommet_j))
+        {
+      
         this.mesElements.get(sommet_i).add(sommet_j);
         this.mesElements.get(sommet_j).add(sommet_i);
+        }
     }
 	//Iterator <String>  monIteration= maliste.iterator();
 	
@@ -71,8 +94,9 @@ public class Graphe
 	  public List<Integer> voisins(int v)
 	  {
 	       if ( verifier_sommet(v))
-
+	    	   
 	        return Collections.unmodifiableList(mesElements.get(v));
+	      
 		    return null;           
 	  }
 	
@@ -90,19 +114,93 @@ public class Graphe
 		  return -1 ;
 	  }
 	  
-	  public int maxDegre() {
-	        //a implementer
-	        return -1;
+	  //degre max d'unn graphe  Iterator <String>  monIteration= maliste.iterator();
+	  public int maxDegre( ArrayList<LinkedList<Integer>> mon_graphe)
+	  {
+		  int i=0; int temp =0, temp1=0,  max =0; 
+		  //LinkedList<Integer> a;
+		  while (i  < nbre_sommet -1)
+		  {
+			 // a=mon_graphe.get(i);
+			  temp=degre(i);
+			  temp1=degre(i+1);
+			  if (temp1>temp)
+				  max=temp1;
+			  else
+			  {
+				  max=temp;
+			  }
+			  
+			  i++;
+		  }
+		  return max;
 	    }
 
 	  
-	  /*
-	   * 
-	   * 
-	   * */
+	  
+	  //somme des degres dans un graphe
+	  public int SommeDegre()
+	  {
+	        int somme=0,i=0;
+	
+	     while (i  < nbre_sommet -1)
+		 {
+				somme=somme+degre(i);	  
+				  i++;
+		 }
+	        
+	        return somme;
+	   }
+	  
+	  //le nombre d'arrete est egale au nombre de la somme des degre divise par 2
+	  public int nbAretes()
+	  {
+		  
+		  return SommeDegre()/2;
+	       
+	  }
+
+	  
+	  // parcours en largeur
+	  
+      void BFS(int s) 
+      { 
+          /** Principe : je marque tout les sommets non visités par defaut tous mes sommets son a FALSE
+           *
+           * */ 
+    	
+          boolean passage[] = new boolean[V]; 
+    
+          // je creer une file 
+          LinkedList<Integer> file = new LinkedList<Integer>(); 
+    
+          // je marque le Noeud courant en mettantr true
+          	
+          passage[s]=true; 
+          file.add(s); 
+    
+          while (file.size() != 0) 
+          { 
+              // tant qu'il ya encore des elements dans la liste defiler 
+              s = file.poll(); 
+              System.out.print(s+" "); 
+    
+              // Get all adjacent vertices of the dequeued vertex s 
+              // If a adjacent has not been visited, then mark it 
+              // visited and enqueue it 
+              
+              Iterator <Integer> i = adjancence[s].listIterator(); 
+              while (i.hasNext()) 
+              { 
+                  int n = i.next(); 
+                  if (!passage[n]) 
+                  { 
+                      passage[n] = true; 
+                      file.add(n); 
+                  } 
+              } 
+          } 
 	  
 	  
-	  
-	  
-	  
+}
 }
